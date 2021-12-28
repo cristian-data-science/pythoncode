8534be6bf6dacc0e6ba467d6ea06896f17e65a05
Cristian Gutierrez <cgutierrez.infor@gmail.com>
2021-12-28 03:02:56 -0300

    'Another update'

--- upgit.py
+++ upgit.py
@@ -2,7 +2,7 @@ import subprocess
 process = subprocess.Popen(["git", "add","."])
 process = subprocess.Popen(["git", "commit","-m", "'Another update'"])
 process = subprocess.Popen(["git", "push","-u", "origin", "master"])
-print(process)
+
 process.wait()
 
 #print("Upload completed!")
